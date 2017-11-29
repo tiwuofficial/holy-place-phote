e7117dced0e443ca84f3e4b8b16ee16d5f6f53d9
tiwu <theimaginationwasunbroken@gmail.com>
2017-11-29 21:33:13 +0900

    add top page

---
+++ src/components/top.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h1>Otak Holy Place Photo</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+section {
+  height: 100vh;
+  width: 100vw;
+}
+h1 {
+  font-weight: normal;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateY(-50%) translateX(-50%);
+  transform: translateY(-50%) translateX(-50%);
+}
+</style>
